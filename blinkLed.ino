ef1ea1438da3f727c63e14802dfed91500b7cdb5
leanhtuanleo <tuananhle18624@gmail.com>
2026-08-04 21:30:05 +0700

    Update blinkLed.ino

--- blinkLed.ino
+++ blinkLed.ino
@@ -1,12 +1,12 @@
 void setup()
 {
-  pinMode(LED_BUILTIN, OUTPUT);
+  pinMode(11, OUTPUT);
 }
 
 void loop()
 {
-  digitalWrite(LED_BUILTIN, HIGH);
+  digitalWrite(11, HIGH);
   delay(1000);     // Wait for 1000 millisecond(s)
-  digitalWrite(LED_BUILTIN, LOW);
+  digitalWrite(11, LOW);
   delay(1000);     // Wait for 1000 millisecond(s)
 }
